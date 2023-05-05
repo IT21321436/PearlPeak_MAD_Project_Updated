1dd0a0953add87c3cbe214ac9440a1b62bc1fbdd
SithiraBuddhika <it21328084@my.sliit.lk>
2023-05-06 00:16:04 +0530

    Integrate Employee Part

---
+++ app/src/main/java/com/example/myapplication/activities/FetchingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.activities
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class FetchingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fetching)
+    }
+}
